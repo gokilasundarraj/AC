11ee68e297bb40436037c4a4285c6397fe22a915
gokilasundarraj <gokilas333@gmail.com>
2026-02-07 16:03:30 +0530

    changes in products

--- client/src/pages/service/ServiceCompleted.js
+++ client/src/pages/service/ServiceCompleted.js
@@ -130,7 +130,7 @@ const ServiceCompleted = () => {
                                 fontWeight: "800",
                               }}
                             >
-                              ₹ {item.price || 0 ? "--" : order.totalPrice}
+                              ₹ {item.price === 0 ? "--" : item.price}
                             </span>
                           </div>
                         ))}
@@ -150,7 +150,7 @@ const ServiceCompleted = () => {
                               fontSize: "1.1rem",
                             }}
                           >
-                            ₹ {order.totalPrice || 0 ? "payment Done" : order.totalPrice }
+                            ₹ {order.totalPrice === 0 ? "payment Done" : order.totalPrice }
                           </strong>
                         </div>
                       </>
